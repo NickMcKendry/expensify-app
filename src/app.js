22b22e31c23aef52625373489c48b98d117b56d8
Nick McKendry <nickmckendry@gmail.com>
2018-04-13 18:48:25 -0600

    added babelrc

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'
+import './styles/styles.scss';
 
-
-
-
-ReactDOM.render(<p>Boiler Plate</p>, document.getElementById('app'))
+ReactDOM.render(<p>Test</p>, document.getElementById('app'))
