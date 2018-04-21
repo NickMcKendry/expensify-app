fe9983bd45c8bb7301bdd652a96e4b8e468790ad
Nick McKendry <nickmckendry@gmail.com>
2018-04-20 23:51:52 -0600

    removed initial dispatch

--- src/app.js
+++ src/app.js
@@ -14,14 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 2000, createdAt: 300 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 5000000, createdAt: 500 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10950, createdAt: 1000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
