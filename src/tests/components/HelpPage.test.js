4cfa69dcc6f15f3fb9fe29f7d80f5f0c608e23a3
Nick McKendry <nickmckendry@gmail.com>
2018-04-21 01:05:04 -0600

    setupd prod build and server

--- src/tests/components/HelpPage.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HelpPage from '../../components/HelpPage';
-import { shallow } from 'enzyme';
-
-test('Should render HelpPage correctly', () => {
-  const wrapper = shallow(<HelpPage />);
-  expect(wrapper).toMatchSnapshot();
-});
